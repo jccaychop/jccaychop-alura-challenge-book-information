26e7864fd55c8c309118b54550a93d3d54c66fe8
jccaychop <jccaychop@outlook.com>
2025-05-23 12:01:37 -0500

    first commit

---
+++ src/main/java/com/alurachallenge/gutendex/GutendexApplication.java
@@ -0,0 +1,18 @@
+package com.alurachallenge.gutendex;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GutendexApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GutendexApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("GutendexApplication running");
+    }
+}
